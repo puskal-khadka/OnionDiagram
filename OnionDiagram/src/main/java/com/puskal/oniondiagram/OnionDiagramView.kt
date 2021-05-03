e274e658c380c3b991574b04980d7953b4215f8e
puskal khadka <puskal.khadka.18@gmail.com>
2021-05-03 14:01:23 +0545

    layer size precise or always unifrom feature addded

--- OnionDiagram/src/main/java/com/puskal/oniondiagram/OnionDiagramView.kt
+++ OnionDiagram/src/main/java/com/puskal/oniondiagram/OnionDiagramView.kt
@@ -26,15 +26,18 @@ class OnionDiagramView : View {
     }
 
     var textAlignPattern: Int = 0
+    var diagramType: Int = 0
 
 
     constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
-        val typedArray: TypedArray = context.obtainStyledAttributes(attrs, R.styleable.OnionDiagramView, 0, 0)
+        val typedArray: TypedArray =
+            context.obtainStyledAttributes(attrs, R.styleable.OnionDiagramView, 0, 0)
         textSize = typedArray.getDimensionPixelSize(R.styleable.OnionDiagramView_textSize, 14)
         textColor = typedArray.getColor(R.styleable.OnionDiagramView_textColor, Color.BLACK)
         bgColor = typedArray.getColor(R.styleable.OnionDiagramView_backgroundColor, Color.WHITE)
 
         textAlignPattern = typedArray.getInt(R.styleable.OnionDiagramView_onionTextAlign, 1)
+        diagramType = typedArray.getInt(R.styleable.OnionDiagramView_diagramType, 0)
         typedArray.recycle()
     }
 
@@ -52,8 +55,9 @@ class OnionDiagramView : View {
 
         if (valueList.isEmpty()) {
             //for testing --> incase if data not send
-             colorList = arrayListOf<Int>(Color.RED, Color.CYAN, Color.YELLOW, Color.MAGENTA, Color.GREEN)
-             valueList = arrayListOf<Float>(300F, 200F, 350F, 150F, 250F)
+            colorList =
+                arrayListOf<Int>(Color.RED, Color.CYAN, Color.YELLOW, Color.MAGENTA, Color.GREEN)
+            valueList = arrayListOf<Float>(300F, 200F, 350F,250F)
         }
 
 
@@ -75,7 +79,7 @@ class OnionDiagramView : View {
         }
 
 
-        var largeRadius = width / 2.toFloat()
+        val largeRadius = width / 2.toFloat()
 
 
         val dataList = arrayListOf<Float>()
@@ -104,9 +108,28 @@ class OnionDiagramView : View {
         for (i in 0 until valueList.size) {
             paint.color = integratedColorList[i]
 
-            mainRadius = (largeRadius / dataList[0]) * dataList[i]
-            xPix = (x / 2) + largeRadius - mainRadius - i * 10
+            when (diagramType) {
+                0 -> {
+                    mainRadius = (largeRadius / dataList[0]) * dataList[i]
+                    xPix = (x / 2) + largeRadius - mainRadius - i * 10
+                }
+                1 -> {
+
+                    if (i != 0) {
+                        mainRadius = mainRadius - (largeRadius / dataList.size) + (largeRadius / (dataList.size * 4F))
+
+                        xPix = ((x / 2) + largeRadius) - mainRadius - i * 10
+
+                    } else {
+                        mainRadius = (largeRadius / dataList[0]) * dataList[i]
+
+                    }
+                }
+            }
+
             canvas?.drawCircle(xPix, yPix, mainRadius, paint)
+
+
 
             paint.textAlign = Paint.Align.LEFT
             paint.setColor(textColor)
@@ -123,8 +146,7 @@ class OnionDiagramView : View {
             } else
                 textXPix = xPix - mainRadius + 8
 
-
-            canvas?.drawText("${dataList[i]}%", textXPix, textYPix, paint)
+            canvas?.drawText("%.1f ".format(dataList[i])+"%", textXPix, textYPix, paint)
 
 
         }
--- app/src/main/java/com/puskal/oniondiagramdemo/MainActivity.kt
+++ app/src/main/java/com/puskal/oniondiagramdemo/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.puskal.oniondiagramdemo
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.puskal.oniondiagram.OnionDiagramView
@@ -21,6 +22,14 @@ class MainActivity : AppCompatActivity() {
          * otherwise default set color will be shown.
          * while defining custom color, if color list size if less than data, then color will be repeated.
          */
-//        onionDiagramView.colorList= arrayListOf(Color.RED,Color.GREEN,Color.Blue)
+//        onionDiagramView.colorList= arrayListOf(Color.RED,Color.GREEN,Color.YELLOW)
+
+        /**NOTE ---------------------->>>>>
+         * Read Carefully below
+         * if you want your onion diagram's layers size according to passed value then use   app:diagramType="CalculatedPrecise" in your xml defined
+         * but in some case you may want your onion layer size should be uniform irrespective to value so in that case use app:diagramType="UniformDecreasing"
+         * By default diagram type is "CalculatedPrecise"
+
+         */
     }
 }
